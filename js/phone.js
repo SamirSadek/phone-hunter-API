b858bc6c1c34ad3eab5ce327f62c8b2d63bbf79d
Samir Sadek <samirsadek2009@gmail.com>
2023-08-29 22:27:07 +0600

    Loading Spinner Added

--- js/phone.js
+++ js/phone.js
@@ -7,33 +7,32 @@ const loadPhone = async (searchText) => {
   displayPhones(phones);
 };
 
-const displayPhones = phones => {
-    // console.log(phones);
+const displayPhones = (phones) => {
+  // console.log(phones);
 
-    const phoneContainer = document.getElementById('phone-container');
-    // clear container cards before adding new cards
+  const phoneContainer = document.getElementById("phone-container");
+  // clear container cards before adding new cards
 
-    phoneContainer.textContent = '';
-    // display Show All Btn
-    const showAllContainer = document.getElementById('show-all-container');
+  phoneContainer.textContent = "";
+  // display Show All Btn
+  const showAllContainer = document.getElementById("show-all-container");
 
-    if(phones.length>12){
-        showAllContainer.classList.remove('hidden')
-    }else{
-        showAllContainer.classList.add('hidden')
-    }
+  if (phones.length > 12) {
+    showAllContainer.classList.remove("hidden");
+  } else {
+    showAllContainer.classList.add("hidden");
+  }
 
-    phones = phones.slice(0,12);
+  phones = phones.slice(0, 12);
 
+  phones.forEach((phone) => {
+    console.log(phone);
+    //2 create a div
+    const phoneCard = document.createElement("div");
+    phoneCard.classList = `card bg-gray-100 shadow-xl`;
 
-    phones.forEach(phone =>{
-        console.log(phone);
-        //2 create a div
-        const phoneCard = document.createElement('div');
-        phoneCard.classList = `card bg-gray-100 shadow-xl`;
-
-        // 3 set inner html
-        phoneCard.innerHTML = `
+    // 3 set inner html
+    phoneCard.innerHTML = `
         <figure><img src="${phone.image}" alt="" /></figure>
             <div class="card-body">
                 <h2 class="card-title">${phone.phone_name}</h2>
@@ -42,20 +41,27 @@ const displayPhones = phones => {
                 <button class="btn btn-primary">Buy Now</button>
                 </div>
             </div>`;
-        // 4 append child
-        phoneContainer.appendChild(phoneCard);
-
-
-    });
-}
+    // 4 append child
+    phoneContainer.appendChild(phoneCard);
+  });
+  toggleLoadingSpinner(false);
+};
 
 // Handle Search Button
 
-const handleSearch = () =>{
- const searchField = document.getElementById('search-field'); 
- const searchText = searchField.value;
- console.log(searchText);
- loadPhone(searchText);
-}
+const handleSearch = () => {
+  toggleLoadingSpinner(true);
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  console.log(searchText);
+  loadPhone(searchText);
+};
 
-
+const toggleLoadingSpinner = (isLoading) => {
+  const loadingSpinner = document.getElementById("loading-spinner");
+  if(isLoading){
+  loadingSpinner.classList.remove("hidden");
+  }else{
+    loadingSpinner.classList.add('hidden')
+  }
+};
