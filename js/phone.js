80413c976c206eb36bf65fa7caf2e9c8810ccf72
Samir Sadek <samirsadek2009@gmail.com>
2023-08-29 20:20:47 +0600

    first commit

---
+++ js/phone.js
@@ -0,0 +1,51 @@
+const loadPhone = async (searchText) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+  );
+  const data = await res.json();
+  const phones = data.data;
+  displayPhones(phones);
+};
+
+const displayPhones = phones => {
+    // console.log(phones);
+
+    const phoneContainer = document.getElementById('phone-container');
+    // clear container cards before adding new cards
+
+    phoneContainer.textContent = '';
+
+
+    phones.forEach(phone =>{
+        console.log(phone);
+        //2 create a div
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card bg-gray-100 shadow-xl`;
+
+        // 3 set inner html
+        phoneCard.innerHTML = `
+        <figure><img src="${phone.image}" alt="" /></figure>
+            <div class="card-body">
+                <h2 class="card-title">${phone.phone_name}</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>`;
+        // 4 append child
+        phoneContainer.appendChild(phoneCard);
+
+
+    });
+}
+
+// Handle Search Button
+
+const handleSearch = () =>{
+ const searchField = document.getElementById('search-field'); 
+ const searchText = searchField.value;
+ console.log(searchText);
+ loadPhone(searchText);
+}
+
+
